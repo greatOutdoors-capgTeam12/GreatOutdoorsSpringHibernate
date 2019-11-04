3f6b78074d770e59df6c6f919aca80b467e6a3fd
Kunal Roychoudhury <lanuks97@gmail.com>
2019-11-04 12:33:18 +0530

    added retailerInventorBean

--- capgemini.go/src/main/java/com/capgemini/go/bean/RetailerInventoryBean.java
+++ capgemini.go/src/main/java/com/capgemini/go/bean/RetailerInventoryBean.java
@@ -1,13 +1,53 @@
 package com.capgemini.go.bean;
 
+import java.time.Period;
+
 public class RetailerInventoryBean {
 	private String retailerId;
-	private String retialerName;
-	
+	private String retailerName;
 	private byte productCategoryNumber;
 	private String productCategoryName;
-	
 	private String productUniqueId;
+	private Period deliveryTimePeriod;
+	private Period shelfTimePeriod;
 	
+	// Getters
+	public String getRetailerId() {return retailerId;}
+	public String getRetialerName() {return retailerName;}
+	public byte getProductCategoryNumber() {return productCategoryNumber;}
+	public String getProductCategoryName() {return productCategoryName;}
+	public String getProductUniqueId() {return productUniqueId;}
+	public Period getDeliveryTimePeriod() {return deliveryTimePeriod;}
+	public Period getShelfTimePeriod() {return shelfTimePeriod;}
 	
+	// Setters
+	public void setRetialerName(String retialerName) {this.retailerName = retialerName;}
+	public void setRetailerId(String retailerId) {this.retailerId = retailerId;}
+	public void setProductCategoryNumber(byte productCategoryNumber) {this.productCategoryNumber = productCategoryNumber;}
+	public void setProductCategoryName(String productCategoryName) {this.productCategoryName = productCategoryName;}
+	public void setProductUniqueId(String productUniqueId) {this.productUniqueId = productUniqueId;}
+	public void setDeliveryTimePeriod(Period deliveryTimePeriod) {this.deliveryTimePeriod = deliveryTimePeriod;}
+	public void setShelfTimePeriod(Period shelfTimePeriod) {this.shelfTimePeriod = shelfTimePeriod;}
+	
+	// Constructors
+	public RetailerInventoryBean () {
+		this.retailerId = null;
+		this.retailerName = null;
+		this.productCategoryNumber = 0;
+		this.productCategoryName = null;
+		this.productUniqueId = null;
+		this.deliveryTimePeriod = null;
+		this.shelfTimePeriod = null;
+	}
+	
+	public RetailerInventoryBean(String retailerId, String retailerName, byte productCategoryNumber,
+			String productCategoryName, String productUniqueId, Period deliveryTimePeriod, Period shelfTimePeriod) {
+		this.retailerId = retailerId;
+		this.retailerName = retailerName;
+		this.productCategoryNumber = productCategoryNumber;
+		this.productCategoryName = productCategoryName;
+		this.productUniqueId = productUniqueId;
+		this.deliveryTimePeriod = deliveryTimePeriod;
+		this.shelfTimePeriod = shelfTimePeriod;
+	}
 }
---
+++ capgemini.go/src/main/java/com/capgemini/go/utility/GoUtility.java
@@ -0,0 +1,10 @@
+package com.capgemini.go.utility;
+
+import java.time.Period;
+import java.util.Calendar;
+
+public class GoUtility {
+	public static Period calculatePeriod (Calendar timestamp1, Calendar timestamp2) {
+		return Period.of(0, 0, 0);
+	}
+}
