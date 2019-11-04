afee4b128593faecfe5ff8697f47c0056be2454b
rintu4mj <55137962+rintu4mj@users.noreply.github.com>
2019-11-04 09:45:40 +0530

    Update HQLQuerryMapper.java

--- capgemini.go/src/main/java/com/capgemini/go/dao/HQLQuerryMapper.java
+++ capgemini.go/src/main/java/com/capgemini/go/dao/HQLQuerryMapper.java
@@ -2,7 +2,7 @@ package com.capgemini.go.dao;
 
 public class HQLQuerryMapper {
 
-	public static final String GET_ALL_PRODUCTS = "FROM ProductEntity prod WHERE prod.quantity >= 0 ORDER BY prod.productName";
+	public static final String GET_ALL_PRODUCTS = "FROM ProductDTO prod WHERE prod.quantity >= 0 ORDER BY prod.productName";
 	
 
 	public static final String USER_ID_EXISTS = "FROM UserEntity WHERE userId =:idExist";
@@ -18,26 +18,45 @@ public class HQLQuerryMapper {
 	public static final String VALIDATE_NUMBER_EMAIL = "SELECT COUNT(*) FROM UserEntity WHERE userNumber =:existNum OR userMail =:existMail";
 
 
+	
+	public static final String IS_ORDER_PRESENT = "FROM OrderDTO WHERE orderId = :ordID";
+	
+
+	public static final String UPDATE_ORDER_PRODUCT_MAP="UPDATE OrderProductMapDTO opm SET opm.productStatus=0 WHERE ORDER_ID=:orderId ";
+
+	
+	
+	
+	
+	//GOADMIN REPORTS HQL QUERRY
+	public static final String SELECT_REVENUE_DATA = "SELECT order.orderInitiateTime , prod.price  FROM OrderDTO order JOIN OrderProductMapDTO opm ON order.orderId=opm.orderId JOIN ProductDTO prod ON opm.productId=prod.productId";
+	
+
+	
+	public static final String SELECT_DATA_FROM_DATABASE = "SELECT order.userId , order.orderInitiateTime , order.orderId , opm.productId , prod.productCategory , prod.price  FROM OrderDTO order JOIN OrderProductMapDTO opm ON order.orderId=opm.orderId JOIN ProductDTO prod ON opm.productId=prod.productId";
+
+	
+	
 }
 
 
 	
-	//public static final String SELECT_DATA_FROM_DATABASE = "SELECT order.userId as ViewSalesReportByUserEntity.userId, order.orderInitiateTime as ViewSalesReportByUserEntity.date, order.orderId as ViewSalesReportByUserEntity.orderId, opm.productId as ViewSalesReportByUserEntity.productId, prod.productCategory as ViewSalesReportByUserEntity.productCategory, prod.price as ViewSalesReportByUserEntity.price FROM OrderEntity order JOIN OrderProductMapEntity opm ON order.orderId=opm.orderId JOIN ProductEntity prod ON opm.productId=prod.productId";
+	//public static final String SELECT_DATA_FROM_DATABASE = "SELECT order.userId as ViewSalesReportByUserEntity.userId, order.orderInitiateTime as ViewSalesReportByUserEntity.date, order.orderId as ViewSalesReportByUserEntity.orderId, opm.productId as ViewSalesReportByUserEntity.productId, prod.productCategory as ViewSalesReportByUserEntity.productCategory, prod.price as ViewSalesReportByUserEntity.price FROM OrderDTO order JOIN OrderProductMapDTO opm ON order.orderId=opm.orderId JOIN ProductDTO prod ON opm.productId=prod.productId";
 	
-	//public static final String SELECT_DATA_FROM_DATABASE = "SELECT order.userId , order.orderInitiateTime , order.orderId , opm.productId , prod.productCategory , prod.price  FROM OrderEntity order JOIN OrderProductMapEntity opm ON order.orderId=opm.orderId JOIN ProductEntity prod ON opm.productId=prod.productId";
+	//public static final String SELECT_DATA_FROM_DATABASE = "SELECT order.userId , order.orderInitiateTime , order.orderId , opm.productId , prod.productCategory , prod.price  FROM OrderDTO order JOIN OrderProductMapDTO opm ON order.orderId=opm.orderId JOIN ProductDTO prod ON opm.productId=prod.productId";
 //	
 	
 	//public static final String SELECT_REVENUE_DATA="SELECT MONTH(`ORDER_INITIATE_TIME`), YEAR(`ORDER_INITIATE_TIME`), `PRODUCT_PRICE`, FROM`ORDER` INNER JOIN `ORDER_PRODUCT_MAP`USING(ORDER_ID) INNER JOIN `PRODUCT` USING (PRODUCT_ID)";
 	
-	public static final String SELECT_REVENUE_DATA = "SELECT order.orderInitiateTime , prod.price  FROM OrderEntity order JOIN OrderProductMapEntity opm ON order.orderId=opm.orderId JOIN ProductEntity prod ON opm.productId=prod.productId";
-
-
-	
-	public static final String SELECT_DATA_FROM_DATABASE = "SELECT order.userId , order.orderInitiateTime , order.orderId , opm.productId , prod.productCategory , prod.price  FROM OrderEntity order JOIN OrderProductMapEntity opm ON order.orderId=opm.orderId JOIN ProductEntity prod ON opm.productId=prod.productId";
-
-	//public static final String SELECT_DATA_FROM_DATABASE = "SELECT order, omp, prod  FROM OrderEntity order JOIN OrderProductMapEntity opm ON order.orderId=opm.orderId JOIN ProductEntity prod ON opm.productId=prod.productId";
+//	public static final String SELECT_REVENUE_DATA = "SELECT order.orderInitiateTime , prod.price  FROM OrderDTO order JOIN OrderProductMapDTO opm ON order.orderId=opm.orderId JOIN ProductDTO prod ON opm.productId=prod.productId";
 //	
-	//public static final String SELECT_DATA_FROM_DATABASE = "FROM OrderEntity";
+//
+//	
+//	public static final String SELECT_DATA_FROM_DATABASE = "SELECT order.userId , order.orderInitiateTime , order.orderId , opm.productId , prod.productCategory , prod.price  FROM OrderDTO order JOIN OrderProductMapDTO opm ON order.orderId=opm.orderId JOIN ProductDTO prod ON opm.productId=prod.productId";
+
+	//public static final String SELECT_DATA_FROM_DATABASE = "SELECT order, omp, prod  FROM OrderDTO order JOIN OrderProductMapDTO opm ON order.orderId=opm.orderId JOIN ProductDTO prod ON opm.productId=prod.productId";
+//	
+	//public static final String SELECT_DATA_FROM_DATABASE = "FROM OrderDTO";
 
 	
 	//"SELECT `ORDER_ID`, `USER_ID` , `ORDER_INITIATE_TIME` FROM ORDER
@@ -46,11 +65,7 @@ public class HQLQuerryMapper {
 		//, , `PRODUCT_UIN` ,  FROM `ORDER` INNER JOIN `ORDER_PRODUCT_MAP` USING (ORDER_ID) INNER JOIN `PRODUCT` USING (PRODUCT_ID)";
 			
 
-	public static final String IS_ORDER_PRESENT = "FROM OrderEntity WHERE orderId = :ordID";
+	
 	
 
-	public static final String UPDATE_ORDER_PRODUCT_MAP="UPDATE OrderProductMapEntity opm SET opm.productStatus=0 WHERE ORDER_ID=:orderId ";
-
-	
-}
 
